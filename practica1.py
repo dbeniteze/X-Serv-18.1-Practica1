d6c5944dce6c24843ed39eb717c8b6386ac2cfba
Diego Benitez Elias <dbenitez@alpha20.aulas.gsyc.urjc.es>
2017-03-14 17:31:20 +0100

    modificaciones pep8

--- practica1.py
+++ practica1.py
@@ -1,11 +1,9 @@
 #! /usr/bin/python3
-#! -*- coding: utf-8 -*-
 
 import webapp
 import sys
 import urllib
 import urllib.parse
-#from urllib.parse import unquote
 
 
 class RecortaUrls(webapp.webApp):
@@ -23,12 +21,11 @@ class RecortaUrls(webapp.webApp):
         """Formulario con las urls introducidas y las acortadas"""
         html = ""
         for keys, values in self.real_a_corta.items():
-            html = (html
-                    + "<a href=" + keys + ">"
-                    + keys + "</a>"
-                    + " ==> "
-                    + "<a href=" + keys + ">"
-                    + values + "</a><br>")
+            html = (html + "<a href=" + keys + ">" +
+                    keys + "</a>" +
+                    " ==> " +
+                    "<a href=" + keys + ">" +
+                    values + "</a><br>")
         return html
 
     def parse(self, request):
@@ -47,40 +44,40 @@ class RecortaUrls(webapp.webApp):
         if metodo == "GET":
             if recurso == "/":
                 httpCode = "200 OK"
-                httpBody = ("<html><body><form action='/' method='POST'>"
-                            + "Recorte una URL <br>"
-                            + "<input type='text' name='url'/></br>"
-                            + "<input type='submit' value='Recorta URL' "
-                            + "/></form><br><br>"
-                            + "<h2>Anteriores URLs acortadas</h2>"
-                            + self.imprimeHTML()
-                            + "</body></html>")
+                httpBody = ("<html><body><form action='/' method='POST'>" +
+                            "Recorte una URL <br>" +
+                            "<input type='text' name='url'/></br>" +
+                            "<input type='submit' value='Recorta URL' " +
+                            "/></form><br><br>" +
+                            "<h2>Anteriores URLs acortadas</h2>" +
+                            self.imprimeHTML() +
+                            "</body></html>")
 
             elif recurso == "/favicon.ico":
                 httpCode = "200 OK"
-                httpBody = ("<html><body><form action='/' method='POST'>"
-                            + "Recorte una URL <br>"
-                            + "<input type='text' name='url'/></br>"
-                            + "<input type='submit' value='Recorta URL' "
-                            + "/></form><br><br>"
-                            + "<h2>Anteriores URLs acortadas</h2>"
-                            + self.imprimeHTML()
-                            + "</body></html>")
+                httpBody = ("<html><body><form action='/' method='POST'>" +
+                            "Recorte una URL <br>" +
+                            "<input type='text' name='url'/></br>" +
+                            "<input type='submit' value='Recorta URL' " +
+                            "/></form><br><br>" +
+                            "<h2>Anteriores URLs acortadas</h2>" +
+                            self.imprimeHTML() +
+                            "</body></html>")
             else:
                 num_recurso = int(recurso.split("/")[1])
-                print (num_recurso)
-                print (self.corta_a_real)
+                print(num_recurso)
+                print(self.corta_a_real)
                 if num_recurso in self.corta_a_real:
                     httpCode = "301"
-                    httpBody = ("<html><body><meta http-equiv='refresh'"
-                                + "content='0;" + " url="
-                                + self.corta_a_real[num_recurso]
-                                + "' /></body></html>")
+                    httpBody = ("<html><body><meta http-equiv='refresh'" +
+                                "content='0;" + " url=" +
+                                self.corta_a_real[num_recurso] +
+                                "' /></body></html>")
                 else:
                     httpCode = "404 Not Found"
-                    httpBody = ("<html><body><h3>"
-                                + "404 Recurso no disponible"
-                                + "</h3></body></html>")
+                    httpBody = ("<html><body><h3>" +
+                                "404 Recurso no disponible" +
+                                "</h3></body></html>")
 
         elif metodo == "POST":
             if recurso == "/":
@@ -91,36 +88,36 @@ class RecortaUrls(webapp.webApp):
                 if prueba != "http":
                     direccion = "http://" + direccion
 
-                if not direccion in self.real_a_corta:
+                if direccion not in self.real_a_corta:
                     url_acortada = self.recortador(url)
                     self.real_a_corta[direccion] = url_acortada
                     self.corta_a_real[self.num_secuencia] = direccion
-                    httpBody = ("<html><body>"
-                                + "<a href=" + direccion + ">"
-                                + direccion + "</a>"
-                                + " ==> "
-                                + "<a href=" + direccion + ">"
-                                + url_acortada + "</a><br>"
-                                + '<input type="button" value="Inicio"'
-                                + 'onClick="location.href' + "='" + url + "'"
-                                + '"/></body></html>')
+                    httpBody = ("<html><body>" +
+                                "<a href=" + direccion + ">" +
+                                direccion + "</a>" +
+                                " ==> " +
+                                "<a href=" + direccion + ">" +
+                                url_acortada + "</a><br>" +
+                                '<input type="button" value="Inicio"' +
+                                'onClick="location.href' + "='" + url + "'" +
+                                '"/></body></html>')
 
                 else:
-                    httpBody = ("<html><body><h3>URL ya acortada \n</h3>"
-                                + "<a href=" + direccion + ">"
-                                + direccion + "</a>"
-                                + " ==> "
-                                + "<a href=" + direccion + ">"
-                                + self.real_a_corta[direccion] + "</a><br>"
-                                + '<input type="button" value="Inicio"'
-                                + 'onClick="location.href' + "='" + url + "'"
-                                + '"/></body></html>')
+                    httpBody = ("<html><body><h3>URL ya acortada \n</h3>" +
+                                "<a href=" + direccion + ">" +
+                                direccion + "</a>" +
+                                " ==> " +
+                                "<a href=" + direccion + ">" +
+                                self.real_a_corta[direccion] + "</a><br>" +
+                                '<input type="button" value="Inicio"' +
+                                'onClick="location.href' + "='" + url + "'" +
+                                '"/></body></html>')
 
         else:
             httpCode = "405 Method Not Allowed"
-            httpBody = ("<html><body><h3>405 Metodo "
-                        + metodo
-                        + " no permitido</h3></body></html>")
+            httpBody = ("<html><body><h3>405 Metodo " +
+                        metodo +
+                        " no permitido</h3></body></html>")
         return (httpCode, httpBody)
 
 if __name__ == '__main__':
